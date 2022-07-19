b046af479f12b4b630eec76124e46fa303da2a97
thebaconfate <62557229+thebaconfate@users.noreply.github.com>
2022-07-19 17:56:56 +0200

    aanpassingen homepagina
    telnr + namen

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   homepageTitle = 'Bierkultuur';
   homepageTitleDescription = 'Promoot de goede smaak sinds 2005!';
-  extraTitleDescription = 'Bier & kaas, Dag van het bier, bierkaffee\'s, cantussen, proeverijen, brouwerijbezoeken en meer!'
+  extraTitleDescription = 'Bier & kaas, Dag van het Bier, Bierkaffee\'s, Cantussen, Proeverijen, Brouwerijbezoeken en meer!'
   homepageIntro = 'Iemand nodig?';
   presidentText = 'teef'
 
